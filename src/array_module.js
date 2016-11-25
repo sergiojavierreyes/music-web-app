d7435d5bb8f32162ec18c79f3761fea6c5b4dd6b
Sergio Reyes <sergio.javier.reyes@me.com>
2016-11-25 17:49:25 +0100

    added videos to database

--- src/array_module.js
+++ src/array_module.js
@@ -1,20 +1,12 @@
-const ypi  			= require ('youtube-playlist-info')
-ypi.playlistInfo("AIzaSyB643_Jt_IPP8c0ElYR43SVYYFyk-e0nd0", "PLTG9OTg_jhUxfzo8aPKvncqQt6RgUQgaK", (playlistItems) => {
-	array = []
-	for (var i = playlistItems.length - 1; i >= 0; i--) {
-		array.push(playlistItems[i].resourceId.videoId)
-	}
-	console.log(array)
-})
-ypi.playlistInfo()
+// const ypi = require ('youtube-playlist-info')
+
+// const arrayStrip = ypi.playlistInfo("AIzaSyB643_Jt_IPP8c0ElYR43SVYYFyk-e0nd0", "PLTG9OTg_jhUxfzo8aPKvncqQt6RgUQgaK", (playlistItems) => {
+// 	array = []
+// 	for (var i = playlistItems.length - 1; i >= 0; i--) {
+// 		array.push(playlistItems[i].resourceId.videoId)
+// 	}
+// 	// console.log(array)
+// })
 
 
-
-
-
-
-
-
-
-
-module.exports = arrayStrip
+// module.exports = arrayStrip
